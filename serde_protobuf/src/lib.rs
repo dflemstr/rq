da9b27464f80d8c943a2fd0b1cda2dc127b788d4
David Flemström <david.flemstrom@gmail.com>
2016-05-23 00:09:18 +0200

    [serde_protobuf] More documentation!

--- serde_protobuf/src/lib.rs
+++ serde_protobuf/src/lib.rs
@@ -1,3 +1,17 @@
+//! Support for [Google protocol buffers][1] in combination with `serde`.
+//!
+//! The crate is split up into several logical parts.
+//!
+//!   * The [`descriptor`](descriptor/index.html) module provides an API for managing dynamically
+//!     loaded protocol buffer schemata.
+//!   * The [`value`](value/index.html) module provides structs that can hold any raw protocol
+//!     buffer decoded data (but the representation is heavily coupled with a schema).
+//!   * The [`de`](de/index.html) module can be used to deserialize binary encoded protocol buffer
+//!     messages given some schema descriptors.
+//!
+//! Serialization is not yet implemented in this version.
+//!
+//! [1]: https://developers.google.com/protocol-buffers/
 extern crate linked_hash_map;
 #[macro_use]
 extern crate log;
