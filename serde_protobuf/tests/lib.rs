2b29fa84d26c0ef701dfda9590d2d7e1521eb9f6
David Flemström <david.flemstrom@gmail.com>
2016-05-22 18:59:48 +0200

    [serde_protobuf] Compile protobuf test suite as part of test

---
+++ serde_protobuf/tests/lib.rs
@@ -0,0 +1,10 @@
+extern crate protobuf;
+extern crate serde;
+extern crate serde_protobuf;
+
+mod protobuf_unittest;
+
+#[test]
+fn it_works() {
+
+}
---
+++ serde_protobuf/tests/protobuf_unittest/mod.rs
@@ -0,0 +1,3 @@
+pub mod unittest;
+pub mod unittest_import;
+pub mod unittest_import_public;
