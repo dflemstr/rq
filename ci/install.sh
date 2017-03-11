657d5e6d1b6777ee18d074ee17cf28a33cd24dcf
David FlemstrÃ¶m <david.flemstrom@gmail.com>
2017-03-11 13:40:19 +0100

    Use custom cross fork that forwards env vars

--- ci/install.sh
+++ ci/install.sh
@@ -4,28 +4,9 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    local target=
-    if [ $TRAVIS_OS_NAME = linux ]; then
-        target=x86_64-unknown-linux-gnu
-        sort=sort
-    else
-        target=x86_64-apple-darwin
-        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
-    fi
+    source ~/.cargo/env || true
 
-    # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
-    echo cross version: $tag
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag $tag \
-           --target $target
+    cargo install --git https://github.com/dflemstr/cross.git --branch env
 
     if [ ! -d v8-build ]
     then
