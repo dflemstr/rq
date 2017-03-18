b2c7c1f3ade3ca00b57abd8b30e13776334f89bc
David Flemström <dflemstr@users.noreply.github.com>
2017-03-18 22:37:58 +0100

    Don't include some files in release tar.gz

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -8,7 +8,7 @@ main() {
     deploy=target/deploy
     revision=$(git describe --tags)
 
-    mkdir -p "$deploy/$TARGET/$revision"
+    mkdir -p "$deploy/$TARGET"
     test -f Cargo.lock || cargo generate-lockfile
 
     export V8_LIBS=$PWD/v8-build/lib/libv8uber.a
@@ -18,15 +18,16 @@ main() {
 
     strip "target/$TARGET/release/rq"
     cp "target/$TARGET/release/rq" "$deploy/$TARGET/rq"
-    cp "target/$TARGET/release/rq" "$deploy/$TARGET/$revision/rq"
+
+    cd $deploy
+    tar czf "$CRATE_NAME-$revision-$TARGET.tar.gz" -- *
 
     curl "https://img.shields.io/badge/${TARGET//-/--}-${revision//-/--}-blue.png" > "$deploy/$TARGET/badge.png"
     curl "https://img.shields.io/badge/${TARGET//-/--}-${revision//-/--}-blue.svg" > "$deploy/$TARGET/badge.svg"
     curl "https://img.shields.io/badge/v-$(echo "$revision" | sed 's/-/--/g;s/v//')-blue.png" > "$deploy/$TARGET/badge-small.png"
     curl "https://img.shields.io/badge/v-$(echo "$revision" | sed 's/-/--/g;s/v//')-blue.svg" > "$deploy/$TARGET/badge-small.svg"
-
-    cd $deploy
-    tar czf "$CRATE_NAME-$revision-$TARGET.tar.gz" -- *
+    mkdir -p "$deploy/$TARGET/$revision"
+    cp "target/$TARGET/release/rq" "$deploy/$TARGET/$revision/rq"
 }
 
 main
