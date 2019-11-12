b611eb77781d370762becf8eb8514a35338e70aa
David Flemström <david.flemstrom@gmail.com>
2019-11-12 20:37:00 +0100

    Don't run CLI as a smoke test any more

--- ci/script.sh
+++ ci/script.sh
@@ -12,9 +12,6 @@ main() {
 
     cross test --target $TARGET
     cross test --target $TARGET --release
-
-    cross run --target $TARGET -- --help
-    cross run --target $TARGET --release -- --help
 }
 
 # we don't run the "test phase" when doing deploys
