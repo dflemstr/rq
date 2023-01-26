ecb5c352b142356db19aba2ebbacdf0685a64693
Julius Michaelis <gitter@liftm.de>
2023-01-26 15:56:41 +0900

    Upgrade to toml = 0.6.0.
    toml 0.6 is based on toml_edit, it:
     - allows to preserve order
     - fixes some mis-serializaitons, e.g. with mixed lists
     - does not require order of non-/table values in maps
    
    This reverts the previous commit.

--- src/value/toml.rs
+++ src/value/toml.rs
@@ -35,8 +35,8 @@ impl value::Source for Source {
     fn read(&mut self) -> error::Result<Option<value::Value>> {
         match self.0.take() {
             Some(v) => {
-                let mut de = toml::de::Deserializer::new(v.as_str());
-                match serde::Deserialize::deserialize(&mut de) {
+                let de = toml::de::Deserializer::new(v.as_str());
+                match serde::Deserialize::deserialize(de) {
                     Ok(v) => Ok(Some(v)),
                     Err(e) => Err(error::Error::from(e)),
                 }
@@ -51,59 +51,15 @@ where
     W: io::Write,
 {
     #[inline]
-    fn write(&mut self, mut value: value::Value) -> error::Result<()> {
-        enforce_toml_output_order(&mut value);
+    fn write(&mut self, value: value::Value) -> error::Result<()> {
         let mut string = String::new();
         {
-            let mut ser = toml::ser::Serializer::new(&mut string);
-            serde::Serialize::serialize(&value, &mut ser)?;
+            let ser = toml::ser::Serializer::new(&mut string);
+            serde::Serialize::serialize(&value, ser)?;
         }
 
         self.0.write_all(string.as_bytes())?;
         self.0.write_all(b"\n")?;
         Ok(())
-    }
-}
-
-fn enforce_toml_output_order(value: &mut value::Value) {
-    match value {
-        value::Value::Sequence(seq) => seq.iter_mut().for_each(enforce_toml_output_order),
-        value::Value::Map(map) => {
-            map.iter_mut()
-                .for_each(|(_, v)| enforce_toml_output_order(v));
-            map.sort_by_key(|(_, v)| Category::of(v));
-        }
-        _ => (),
-    }
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-enum Category {
-    Primitive,
-    Array,
-    Table,
-}
-
-impl Category {
-    fn of(value: &value::Value) -> Category {
-        match value {
-            value::Value::Unit
-            | value::Value::Bool(_)
-            | value::Value::I8(_)
-            | value::Value::I16(_)
-            | value::Value::I32(_)
-            | value::Value::I64(_)
-            | value::Value::U8(_)
-            | value::Value::U16(_)
-            | value::Value::U32(_)
-            | value::Value::U64(_)
-            | value::Value::F32(_)
-            | value::Value::F64(_)
-            | value::Value::Char(_)
-            | value::Value::String(_)
-            | value::Value::Bytes(_) => Category::Primitive,
-            value::Value::Sequence(_) => Category::Array,
-            value::Value::Map(_) => Category::Table,
-        }
     }
 }
